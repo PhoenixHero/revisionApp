db9dd1a5020933c5702435947ae008faf5b2ff46
PhoenixHero <henry_willee@hotmail.com>
2022-06-12 16:53:08 +1000

    setup

--- file.h
+++ file.h
@@ -1,12 +1,23 @@
-#pragma once;
 #include <iostream>
 #include <string>
 #include <map>
 #include <random>
 #include <time.h>
+#include <fstream>
 
 std::map<std::string, std::string> list;
 std::map<std::string, int> prog;
 std::map<int, std::string> place;
 
-void in(std::string file);
+
+std::string ln;
+void in(std::string file)
+{
+    std::ifstream files(file);
+    while ( getline (files,ln) )
+    {
+    //Printing the output of the file contents
+    std::cout << ln << '\n';
+    }
+    files.close();
+}
